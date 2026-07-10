71522be56ac914dd57f34ec820356d4e43445e41
Athul-krish <athulkrishnapanamakkal@gmail.com>
2026-07-10 11:01:38 +0530

    Json added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Product-App</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Product-App</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="/add">Add Product</a>
-        <a class="nav-link" href="/search">Search</a>
-        <a class="nav-link" href="delete">Delete</a>
-        <a class="nav-link" href="/viewall">View All</a>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="/add">Add Product</a>
+        <a className="nav-link" href="/search">Search</a>
+        <a className="nav-link" href="delete">Delete</a>
+        <a className="nav-link" href="/viewall">View All</a>
       </div>
     </div>
   </div>
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 
 const ViewAll = () => {
+
+  const [data,changeData] = useState([
+  {
+    "name": "Samsung Galaxy F70e 5G",
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqRhAcUd3v22BRyhbma7eqaXw3peJjmRn8odVz0GIZtA&s",
+    "desc": "6.7-inch AMOLED display, Exynos processor, 50MP triple camera, 5000mAh battery with 25W fast charging."
+  },
+  {
+    "name": "Ai+ Pulse 5G",
+    "img": "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/7/g/a/-original-imahh27g4e28qac6.jpeg?q=70",
+    "desc": "Affordable 5G smartphone with a smooth 120Hz display, AI camera features, and long-lasting battery life."
+  },
+  {
+    "name": "Motorola Edge 70 Pro+",
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-gHBpYdyqIgHKITK-uvNdVTe2KfDJHVCII4Dr5HNQFg&s=10",
+    "desc": "Premium flagship featuring a curved OLED display, Snapdragon chipset, 200MP camera, and ultra-fast charging."
+  },
+  {
+    "name": "POCO C85x",
+    "img": "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/q/7/s/-original-imahmqganju6a3gt.jpeg?q=70",
+    "desc": "Budget-friendly smartphone with a large HD+ display, efficient processor, and massive 5000mAh battery."
+  },
+  {
+    "name": "Samsung Crystal Vision 4K Smart TV",
+    "img": "https://rukminim2.flixcart.com/image/312/312/xif0q/television/a/g/0/-original-imahj8ydqperu79z.jpeg?q=70",
+    "desc": "43-inch 4K UHD Smart TV with HDR support, Crystal Processor, and built-in streaming apps."
+  },
+  {
+    "name": "LG UHD AI Smart TV",
+    "img": "https://rukminim2.flixcart.com/image/312/312/xif0q/television/r/u/i/-original-imahn8y5rfgknhnc.jpeg?q=70",
+    "desc": "4K Ultra HD Smart TV powered by AI ThinQ with vivid colors and immersive surround sound."
+  },
+  {
+    "name": "Sony BRAVIA X75L",
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNO36XKY6a7gCZq0CK5bQrqrMjc-VhMWm9QQVwp4TNoQ&s=10",
+    "desc": "Google TV with 4K HDR, X1 Processor, Dolby Audio, and voice-enabled remote control."
+  },
+  {
+    "name": "OnePlus Nord CE 5G",
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO0Hh_Rjs7z4Soe4g6CpLP2Cc2kAIDLTsuEQe58n2SDg&s=10",
+    "desc": "Slim and lightweight smartphone featuring AMOLED display, 64MP camera, and Warp Charge technology."
+  },
+  {
+    "name": "Redmi Note 14 Pro",
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyhqbIxSu6xTcnd06O_16kfhSnfiwjXvv_n1LePLbZJQ&s=10",
+    "desc": "High-performance smartphone with 120Hz AMOLED display, 200MP AI camera, and fast charging support."
+  },
+  {
+    "name": "realme Narzo 80 Pro",
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLIm3LPtbMWYI44KUl6-D284VOYYnOmK0S_LQgqdh0Wg&s=10",
+    "desc": "Gaming-focused smartphone powered by a MediaTek processor with smooth performance and extended battery life."
+  },
+  {
+    "name": "vivo V50 5G",
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJhSMmlrT8SWHrbDG_-1T7NXNlj1SLNekrz8lcZC_Wsw&s=10",
+    "desc": "Elegant smartphone with ZEISS camera technology, vibrant AMOLED display, and reliable all-day battery."
+  },
+  {
+    "name": "OPPO Reno 14 5G",
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAN_ZqGHFHlICnFwCJjRicONciTe4mqpEBMskNq8cxiA&s=10",
+    "desc": "Stylish smartphone featuring AI portrait camera, fast charging, premium design, and smooth performance."
+  }
+]);
+
   return (
     <div>
       <Navbar />
@@ -9,19 +73,22 @@ const ViewAll = () => {
         <div className="row">
           <div className="col col-12">
             <div className="row g-3">
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+
+
+              {data.map((value,index)=>{
+                return(
+                <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
                 <div class="card">
                   <img
                     height={300}
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqRhAcUd3v22BRyhbma7eqaXw3peJjmRn8odVz0GIZtA&s"
+                    src={value.img}
                     class="card-img-top"
                     alt="..."
                   />
                   <div class="card-body">
-                    <h5 class="card-title">Samsung Galaxy F70e 5G</h5>
+                    <h5 class="card-title">{value.name}</h5>
                     <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
+                      {value.desc}
                     </p>
                     <a href="#" class="btn btn-primary">
                       Go somewhere
@@ -29,182 +96,8 @@ const ViewAll = () => {
                   </div>
                 </div>
               </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
-                <div class="card">
-                  <img height={300} src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/7/g/a/-original-imahh27g4e28qac6.jpeg?q=70" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Ai+ Pulse 5G</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
-                <div class="card">
-                  <img  height={300} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-gHBpYdyqIgHKITK-uvNdVTe2KfDJHVCII4Dr5HNQFg&s=10" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Motorola Edge 70 Pro+</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
-                <div class="card">
-                  <img height={300} src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/q/7/s/-original-imahmqganju6a3gt.jpeg?q=70" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">poco C85x</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
-                <div class="card">
-                  <img height={300} src="https://rukminim2.flixcart.com/image/312/312/xif0q/television/a/g/0/-original-imahj8ydqperu79z.jpeg?q=70" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
-                <div class="card">
-                  <img height={300} src="https://rukminim2.flixcart.com/image/312/312/xif0q/television/r/u/i/-original-imahn8y5rfgknhnc.jpeg?q=70" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
-                <div class="card">
-                  <img height={300} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNO36XKY6a7gCZq0CK5bQrqrMjc-VhMWm9QQVwp4TNoQ&s=10" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
-                <div class="card">
-                  <img height={300} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO0Hh_Rjs7z4Soe4g6CpLP2Cc2kAIDLTsuEQe58n2SDg&s=10" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
-                <div class="card">
-                  <img height={300} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyhqbIxSu6xTcnd06O_16kfhSnfiwjXvv_n1LePLbZJQ&s=10" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
-                <div class="card">
-                  <img height={300} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLIm3LPtbMWYI44KUl6-D284VOYYnOmK0S_LQgqdh0Wg&s=10" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
-                <div class="card">
-                  <img height={300} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJhSMmlrT8SWHrbDG_-1T7NXNlj1SLNekrz8lcZC_Wsw&s=10" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
-                <div class="card">
-                  <img height={300} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAN_ZqGHFHlICnFwCJjRicONciTe4mqpEBMskNq8cxiA&s=10" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card’s content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
+                )
+              })}
             </div>
           </div>
         </div>
